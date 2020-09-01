9e9665113ce80dfdab203cdeb8993e2e3e8c297c
lucasbrustolin <lucasbrustolin@hotmail.com>
2020-09-01 01:52:53 -0300

    Criando arquivo texto e adicionando texto a um arquivo ja existente

--- criandotxt.py
+++ criandotxt.py
@@ -3,6 +3,7 @@
 #
 # Description: Criando e escrevendo arquivo texto no modo 'W'
 # -------------------------------------------------------------------------------------
+"""
 arquivo = open('resource/arquivocriado.txt','w')
 arquivo.write('Este arquivo texto foi gerado via Python - metodo Open() = modo (w)\n')
 arquivo.write('Arquivo texto ..................\n')
@@ -11,7 +12,27 @@ arquivo.close()
 
 #Lendo arquivo criado
 print("\nLendo arquivo criado via Python\n")
-arquivo = open('resource/arquivocriado.txt')
+arquivo = open('resource/arquivocriado.txt','r')
+for linha in arquivo:
+    linha = linha.rstrip()
+    print(linha)
+arquivo.close()
+"""
+# -------------------------------------------------------------------------------------
+#                                       EXEMPLO 2
+#
+# Description: Escrevendo em um arquivo ja criado - modo 'a'
+# -------------------------------------------------------------------------------------
+print("\n")
+texto = input("Digite um texto para ser acrescentado ao arquivo criado\n")
+arquivo = open('resource/arquivocriado.txt','a')
+arquivo.write( texto + '\n')
+print('Operacao de adicao de texto efetuada com sucesso no arquivo:' + arquivo.name + '\n'   )
+arquivo.close()
+
+#Lendo arquivo criado
+print("\nLendo o texto adiconado ao arquivo!\n")
+arquivo = open('resource/arquivocriado.txt','r')
 for linha in arquivo:
     linha = linha.rstrip()
     print(linha)
